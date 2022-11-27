e63188b82a9c23b130369c614531267a6bfc1cb6
shiroul <mjamiulakbar@gmail.com>
2022-11-27 18:29:51 +0700

    delelte console log

--- checkWin.js
+++ checkWin.js
@@ -5,27 +5,20 @@ export class CheckWin{
 
     checkWin(assign) {
         if(assign[0] == this.player && assign[1] == this.player && assign[2] == this.player){
-            console.log('yeyeaysy')
             return true;
         }
         if(assign[3] == this.player && assign[4] == this.player && assign[5] == this.player){
-            console.log('yeyeaysy')
             return true;
         }
         if(assign[6] == this.player && assign[7] == this.player && assign[8] == this.player){
-            console.log('yeyeaysy')
             return true;
         }
         if(assign[0] == this.player && assign[4] == this.player && assign[8] == this.player){
-            console.log('yeyeaysy')
             return true;
         }
         if(assign[2] == this.player && assign[4] == this.player && assign[6] == this.player){
-            console.log('yeyeaysy')
             return true;
         }
-        console.log(assign)
-        console.log('lah')
         return false;
     }
 }
